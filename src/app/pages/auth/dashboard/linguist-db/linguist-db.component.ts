6f2611c7df7194633bbdeedf1ac18445ed7d1f21
Gregorio Giorgino <giorgino.greg@gmail.com>
2025-10-03 13:15:28 +0200

    extend linguist dashboard

--- src/app/pages/auth/dashboard/linguist-db/linguist-db.component.ts
+++ src/app/pages/auth/dashboard/linguist-db/linguist-db.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CoursePreviewComponent } from '@app/pages/auth/dashboard/learner-db/components/coursePreview/coursePreview.component';
+import { CreateNewCourseRoute } from '@app/routes/basicRoutes';
+import { ButtonComponent } from '@app/shared/components/ui/button/button.component';
 
 @Component({
   selector: 'app-linguist-db',
-  imports: [],
+  imports: [ButtonComponent, CoursePreviewComponent],
   templateUrl: './linguist-db.component.html',
   styleUrl: './linguist-db.component.scss',
 })
-export class LinguistDashboardComponent {}
+export class LinguistDashboardComponent {
+  constructor(private router: Router) {}
+  allCourses = [];
+  availableCoursesToUnlock = [];
+
+  onCreateNewCourse() {
+    this.router.navigate([CreateNewCourseRoute.path]);
+  }
+}
